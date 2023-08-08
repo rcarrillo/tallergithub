d7d72c6dfb1060694da0477221c3eef583f59783
Ramón Carrillo <rcarrillo@duodata.io>
2023-08-08 15:41:07 -0500

    Mejora mensajes: signo de admiración

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-print("Bienvenido!")
-print("Hola mundo!")
+print("¡Bienvenido!")
+print("¡Hola mundo!")
 
-print("Taller git")
+print("Taller git:")
 x = 1 + 2
 print(x)
 
 for i in range(10):
     print(i)
 
-print('Mensaje final!')
+print('¡Mensaje final!')
